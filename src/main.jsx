1a0cab865531bbfba58f620781eef2fbcaa7325a
Dhanush <sridhanush874@gmail.com>
2025-07-26 10:14:17 +0530

    Fix: Updated main.jsx to use App1 instead of App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import App1 from './App1.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   // strict mode is used to render two times
